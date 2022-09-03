7d0c614756a43292da357625c4c7a0209a797261
Berus Frager <gitmailru@yandex.ru>
2022-09-03 10:31:32 +0300

    Коммит файлов папки проекта Example069

---
+++ Example069/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 69:
+// Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+Console.Clear();
+Console.Write("Введите число А - основание степени: ");
+int basis = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В - показатель степени: ");
+int indicator = Convert.ToInt32(Console.ReadLine());
+int Exponentiation (int basis, int indicator)
+{
+    if(indicator == 0) return 1;
+    return basis * Exponentiation(basis, indicator - 1);
+}
+Console.Write(basis + " в степени " + indicator + " равно " + Exponentiation(basis, indicator) + ".");
