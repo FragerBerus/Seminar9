52d0a7286ecfd3d79203518c789e1df08911db4c
Berus Frager <gitmailru@yandex.ru>
2022-09-03 10:07:43 +0300

    Коммит файлов папки проекта Example067

---
+++ Example067/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67:
+// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.Clear();
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigits = 0;
+int SumDigit (int number, int sumDigits)
+{
+    if (number / 10 == 0) return number;
+    return sumDigits + number % 10 + SumDigit(number / 10, sumDigits);
+}
+Console.WriteLine(SumDigit(number, sumDigits));
+// Console.WriteLine("Сумма цифр числа " + number + " равна " + sumDigits + ".");
